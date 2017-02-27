0b7a5d13cf1224d222249c322756a73d39705231
Vania Alfitri <alfitriv@gmail.com>
2017-02-27 00:14:15 -0800

    Initial check in for UI_automated testing project

---
+++ UI_testing/UI_testing/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace UI_testing
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			IWebDriver driver = new ChromeDriver("/Users/valfitri/projects/UI_testing/UI_testing");
+			driver.Navigate().GoToUrl("http://www.cisvpdx.com/");
+			//driver.Url = "www.cisvpdx.com";
+			//driver.Navigate().GoToUrl("http://www.google.com/");
+
+		}
+	}
+}
---
+++ anewbeginning/anewbeginning/HelloWorld.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace anewbeginning
+{
+	public class HelloWorld
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Hello World");
+		}
+	}
+}
