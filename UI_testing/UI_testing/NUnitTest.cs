6f1fbc1e2fb894d46c744d2e2bc1d9d86d87c332
Vania Alfitri <alfitriv@gmail.com>
2017-03-14 19:32:36 -0700

    added NUnit framework

---
+++ UI_testing/UI_testing/NUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using System;
+namespace UI_testing
+{
+	public class NUnitTest
+	{
+		IWebDriver driver;
+
+		[SetUp]
+		public void Initialize()
+		{
+			driver = new ChromeDriver("/Users/valfitri/projects/UI_testing/UI_testing");
+		}
+
+		[Test]
+		//Open the application
+		public void OpenAppTest()
+		{
+			driver.Navigate().GoToUrl("http://www.cisvpdx.com/");
+		}
+
+		[TearDown]
+		//Close the browser
+		public void EndTest()
+		{
+			driver.Close();
+		}
+	}
+}
